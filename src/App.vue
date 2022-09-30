5ac8aadb755f7b5d7b7619e09305af3c76bb9806
Nabiel Alif <nabiel.alif01@gmail.com>
2022-09-30 16:57:48 +0700

    commitin

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,20 @@
 <script>
-import Navbar from "./components/Navbar.vue";
-import { RouterView } from "vue-router";
+import NavBar from "./components/NavBar.vue";
+import Jumbotron from "./components/Jumbotron.vue";
+import Footer from "./components/Footer.vue";
+import HomeView from "./views/HomeView.vue";
 
 export default {
-  components: { Navbar },
+  components: { NavBar, Jumbotron, Footer, HomeView },
 };
 </script>
 
 <template>
-  <Navbar></Navbar>
+  <NavBar />
+  <Jumbotron />
 
-  <!-- jumbotron -->
-  <section class="mx-auto grid grid-cols-3 w-3/4 border border-black">
-    <button class="btn">Button</button>
-    <div class="col-start-1 col-end-3 border border-blue-400">
-      <img
-        src="https://www.thebluesville.com/wp-content/uploads/2022/03/DSCF9911-scaled.jpg"
-        alt=""
-        class="max-h-72 min-h-max"
-      />
-    </div>
-
-    <div class="w-full border border-blue-400">
-      <h1 class="text-3xl">Indigo Dyeing</h1>
-      <p>
-        Admin BV On 15/03/2022 Indigo Dyeing We are doing some experiment in
-        Indigo Dyeing. Yesterday we had fun by indigo dyeing some of our wooden
-        bowl. The result looks amazing and interesting. The color may not to
-        bold, but...
-      </p>
-      <!-- <img
-        class="object-cover"
-        src="https://www.thebluesville.com/wp-content/uploads/2022/03/AH0A6900-1-scaled.jpg"
-        alt=""
-      /> -->
-    </div>
-  </section>
+  <HomeView />
+  <Footer />
 
   <RouterView />
 </template>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,57 @@
+<script></script>
+<template>
+  <div class="carousel rounded-md w-1/2 mx-auto">
+    <div id="slide1" class="carousel-item relative w-full">
+      <img
+        src="https://www.thebluesville.com/wp-content/uploads/2022/03/DSCF9911-scaled.jpg"
+        class="object-cover"
+      />
+      text
+      <div
+        class="absolute text-5xl text-red-500 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      >
+        <h1>Lorem</h1>
+
+        <p
+          class="absolute text-sm text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        >
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi quas
+          distinctio culpa facere laboriosam temporibus odit quos deleniti
+          maxime velit harum blanditiis ratione omnis rem, aut ipsum
+          consequuntur corporis dolorem.
+        </p>
+      </div>
+      <div
+        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+      >
+        <a href="#slide4" class="btn btn-circle">❮</a>
+        <a href="#slide2" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+    <div id="slide2" class="carousel-item relative w-full">
+      <img
+        src="https://www.thebluesville.com/wp-content/uploads/2020/02/DSC00395.jpg"
+        class="w-full"
+      />
+      <div
+        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+      >
+        <a href="#slide1" class="btn btn-circle">❮</a>
+        <a href="#slide3" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+    <div id="slide3" class="carousel-item relative w-full">
+      <img
+        src="https://www.thebluesville.com/wp-content/uploads/2022/09/000019-min-1.png"
+        class="w-full"
+      />
+      <div
+        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+      >
+        <a href="#slide2" class="btn btn-circle">❮</a>
+        <a href="#slide4" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+  </div>
+</template>
+<style></style>
